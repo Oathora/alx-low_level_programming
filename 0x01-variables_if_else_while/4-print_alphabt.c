76566befeb7bfa7f0be4c5a5daec171705222283
athora omenuwoma <athoraomenuwoma@gmail.com>
2023-03-17 07:27:14 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

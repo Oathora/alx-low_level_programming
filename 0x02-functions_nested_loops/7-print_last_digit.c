ac14d2622b8841a1b8fba75d8ba8367a3636003b
athora omenuwoma <athoraomenuwoma@gmail.com>
2023-03-21 12:38:04 -0700

    C Functions, nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the int to extract the last digit from
@@ -7,15 +8,15 @@
  */
 int print_last_digit(int n)
 {
-
-	int a;
+	int a;	
 
 	if (n < 0)
 	n = -n;
-	a = n % 10;	
+	a = n % 10;
 	if (a < 0)
 	a = -a;
+	
 	_putchar(a + '0');
-	return (a);
+        return (a);
 		
 }
